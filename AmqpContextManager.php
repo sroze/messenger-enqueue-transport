Gnucki/messenger-adapter

Add AMQP topic exchange handling

AmqpContextManager.php
@@ -58,15 +58,19 @@ class AmqpContextManager implements ContextManager
         }
 
         $topic = $this->psrContext->createTopic($destination['topic']);
-        $topic->setType(AmqpTopic::TYPE_FANOUT);
-        $topic->addFlag(AmqpTopic::FLAG_DURABLE);
+        $topic->setType($destination['topicOptions']['type'] ?? AmqpTopic::TYPE_FANOUT);
+        $topicFlags = $destination['topicOptions']['flags'] ?? ((int) $topic->getFlags() | AmqpTopic::FLAG_DURABLE);
+        $topic->setFlags($topicFlags);
         $this->psrContext->declareTopic($topic);
 
         $queue = $this->psrContext->createQueue($destination['queue']);
-        $queue->addFlag(AmqpQueue::FLAG_DURABLE);
+        $queueFlags = $destination['queueOptions']['flags'] ?? ((int) $queue->getFlags() | AmqpQueue::FLAG_DURABLE);
+        $queue->setFlags($queueFlags);
         $this->psrContext->declareQueue($queue);
 
-        $this->psrContext->bind(new AmqpBind($queue, $topic));
+        $this->psrContext->bind(
+            new AmqpBind($queue, $topic, $destination['queueOptions']['bindingKey'] ?? null)
+        );
 
         return true;
     }

QueueInteropTransport.php
@@ -21,6 +21,8 @@ use Symfony\Component\Messenger\Transport\Serialization\DecoderInterface;
 use Symfony\Component\Messenger\Transport\Serialization\EncoderInterface;
 use Symfony\Component\Messenger\Transport\TransportInterface;
 use Interop\Queue\Exception as InteropQueueException;
+use Interop\Queue\PsrMessage;
+use Enqueue\MessengerAdapter\Exception\MissingMessageMetadataSetterException;
 use Enqueue\MessengerAdapter\Exception\SendingMessageFailedException;
 use Enqueue\MessengerAdapter\EnvelopeItem\TransportConfiguration;
 use Symfony\Component\OptionsResolver\Options;
@@ -120,6 +122,8 @@ class QueueInteropTransport implements TransportInterface
             $encodedMessage['headers'] ?? array()
         );
 
+        $this->setMessageMetadata($psrMessage, $message);
+
         $producer = $psrContext->createProducer();
 
         if (isset($this->options['deliveryDelay'])) {
@@ -188,7 +192,29 @@ class QueueInteropTransport implements TransportInterface
 
         return array(
             'topic' => $topic ?? $this->options['topic']['name'],
+            'topicOptions' => $this->options['topic'],
             'queue' => $this->options['queue']['name'],
+            'queueOptions' => $this->options['queue'],
         );
+    }
+
+    private function setMessageMetadata(PsrMessage $psrMessage, Envelope $message): void
+    {
+        $configuration = $message->get(TransportConfiguration::class);
+
+        if (null === $configuration) {
+            return;
+        }
+
+        $metadata = $configuration->getMetadata();
+        $class = new \ReflectionClass($psrMessage);
+
+        foreach ($metadata as $key => $value) {
+            $setter = sprintf('set%s', ucfirst($key));
+            if (!$class->hasMethod($setter)) {
+                throw new MissingMessageMetadataSetterException($key, $setter, $class->getName());
+            }
+            $psrMessage->{$setter}($value);
+        }
     }
 }

Tests/AmqpContextManagerTest.php
@@ -1,0 +1,129 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Enqueue\MessengerAdapter\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Interop\Amqp\AmqpContext;
+use Interop\Amqp\AmqpQueue;
+use Interop\Amqp\AmqpTopic;
+use Interop\Amqp\Impl\AmqpBind;
+use Interop\Queue\PsrContext;
+use Enqueue\MessengerAdapter\AmqpContextManager;
+
+class AmqpContextManagerTest extends TestCase
+{
+    private function getContextManager($topicName, $queueName, $topicType = null, $topicFlags = null, $queueBindingKey = null, $queueFlags = null)
+    {
+        $topicProphecy = $this->prophesize(AmqpTopic::class);
+        $topicProphecy->setType($topicType ?? AmqpTopic::TYPE_FANOUT)->shouldBeCalled();
+        if (null === $topicFlags) {
+            $topicProphecy->getFlags()->shouldBeCalled()->willReturn(AmqpTopic::FLAG_PASSIVE);
+        }
+        $topicProphecy->setFlags($topicFlags ?? (AmqpTopic::FLAG_DURABLE | AmqpTopic::FLAG_PASSIVE))->shouldBeCalled();
+        $topic = $topicProphecy->reveal();
+
+        $queueProphecy = $this->prophesize(AmqpQueue::class);
+        if (null === $queueFlags) {
+            $queueProphecy->getFlags()->shouldBeCalled()->willReturn(AmqpQueue::FLAG_PASSIVE);
+        }
+        $queueProphecy->setFlags($queueFlags ?? (AmqpQueue::FLAG_DURABLE |AmqpQueue::FLAG_PASSIVE))->shouldBeCalled();
+        $queue = $queueProphecy->reveal();
+
+        $bind = new AmqpBind($queue, $topic, $queueBindingKey);
+
+        $contextProphecy = $this->prophesize(AmqpContext::class);
+        $contextProphecy->createTopic($topicName)->shouldBeCalled()->willReturn($topic);
+        $contextProphecy->declareTopic($topic)->shouldBeCalled();
+        $contextProphecy->createQueue($queueName)->shouldBeCalled()->willReturn($queue);
+        $contextProphecy->declareQueue($queue)->shouldBeCalled();
+        $contextProphecy->bind($bind)->shouldBeCalled();
+        $context = $contextProphecy->reveal();
+
+        return new AmqpContextManager($context);
+    }
+
+    public function testDefaultEnsure()
+    {
+        $topicName = 'foo';
+        $queueName = 'bar';
+        $contextManager = $this->getContextManager($topicName, $queueName);
+
+        $this->assertTrue($contextManager->ensureExists(array(
+            'topic' => $topicName,
+            'topicOptions' => array('name' => $topicName),
+            'queue' => $queueName,
+            'queueOptions' => array('name' => $queueName),
+        )));
+    }
+
+    public function testCustomizedEnsure()
+    {
+        $topicName = 'foo';
+        $queueName = 'bar';
+        $topicType = 'topic';
+        $topicFlags = '8';
+        $queueFlags = 16;
+        $queueBindingKey = 'foo.#';
+        $contextManager = $this->getContextManager($topicName, $queueName, $topicType, $topicFlags, $queueBindingKey, $queueFlags);
+
+        $this->assertTrue($contextManager->ensureExists(array(
+            'topic' => $topicName,
+            'topicOptions' => array('name' => $topicName, 'type' => $topicType, 'flags' => $topicFlags),
+            'queue' => $queueName,
+            'queueOptions' => array('name' => $queueName, 'bindingKey' => $queueBindingKey, 'flags' => $queueFlags),
+        )));
+    }
+
+    public function testNotProcessedEnsure()
+    {
+        $topicName = 'foo';
+        $queueName = 'bar';
+
+        $contextProphecy = $this->prophesize(PsrContext::class);
+        $context = $contextProphecy->reveal();
+
+        $contextManager = new AmqpContextManager($context);
+
+        $this->assertFalse($contextManager->ensureExists(array(
+            'topic' => $topicName,
+            'topicOptions' => array('name' => $topicName),
+            'queue' => $queueName,
+            'queueOptions' => array('name' => $queueName),
+        )));
+    }
+
+    public function testContextRetrieving()
+    {
+        $contextProphecy = $this->prophesize(PsrContext::class);
+        $context = $contextProphecy->reveal();
+
+        $contextManager = new AmqpContextManager($context);
+
+        $this->assertSame($context, $contextManager->psrContext());
+    }
+
+    /* public function testExceptionRecovering()
+    {
+        $topicName = 'foo';
+        $queueName = 'bar';
+        $contextManager = $this->getContextManager($topicName, $queueName);
+
+        $exception = new \AMQPQueueException('', 404); // Class 'AMQPQueueException' not found
+
+        $this->assertTrue($contextManager->recoverException($exception, array(
+            'topic' => $topicName,
+            'topicOptions' => array('name' => $topicName),
+            'queue' => $queueName,
+            'queueOptions' => array('name' => $queueName),
+        )));
+    } */
+}

sroze/messenger-enqueue-transport

AmqpContextManager.php
@@ -58,15 +58,19 @@ class AmqpContextManager implements ContextManager
         }
 
         $topic = $this->context->createTopic($destination['topic']);
-        $topic->setType(AmqpTopic::TYPE_FANOUT);
-        $topic->addFlag(AmqpTopic::FLAG_DURABLE);
+        $topic->setType($destination['topicOptions']['type'] ?? AmqpTopic::TYPE_FANOUT);
+        $topicFlags = $destination['topicOptions']['flags'] ?? ((int) $topic->getFlags() | AmqpTopic::FLAG_DURABLE);
+        $topic->setFlags($topicFlags);
         $this->context->declareTopic($topic);
 
         $queue = $this->context->createQueue($destination['queue']);
-        $queue->addFlag(AmqpQueue::FLAG_DURABLE);
+        $queueFlags = $destination['queueOptions']['flags'] ?? ((int) $queue->getFlags() | AmqpQueue::FLAG_DURABLE);
+        $queue->setFlags($queueFlags);
         $this->context->declareQueue($queue);
 
-        $this->context->bind(new AmqpBind($queue, $topic));
+        $this->context->bind(
+            new AmqpBind($queue, $topic, $destination['queueOptions']['bindingKey'] ?? null)
+        );
 
         return true;
     }

QueueInteropTransport.php
@@ -20,6 +20,8 @@ use Symfony\Component\Messenger\Envelope;
 use Symfony\Component\Messenger\Transport\Serialization\SerializerInterface;
 use Symfony\Component\Messenger\Transport\TransportInterface;
 use Interop\Queue\Exception as InteropQueueException;
+use Interop\Queue\Message;
+use Enqueue\MessengerAdapter\Exception\MissingMessageMetadataSetterException;
 use Enqueue\MessengerAdapter\Exception\SendingMessageFailedException;
 use Enqueue\MessengerAdapter\EnvelopeItem\TransportConfiguration;
 use Symfony\Component\OptionsResolver\Options;
@@ -115,11 +117,14 @@ class QueueInteropTransport implements TransportInterface
 
         $encodedMessage = $this->serializer->encode($message);
 
+        $originalMessage = $message;
         $message = $context->createMessage(
             $encodedMessage['body'],
             $encodedMessage['properties'] ?? array(),
             $encodedMessage['headers'] ?? array()
         );
+
+        $this->setMessageMetadata($message, $originalMessage);
 
         $producer = $context->createProducer();
 
@@ -195,7 +200,29 @@ class QueueInteropTransport implements TransportInterface
 
         return array(
             'topic' => $topic ?? $this->options['topic']['name'],
+            'topicOptions' => $this->options['topic'],
             'queue' => $this->options['queue']['name'],
+            'queueOptions' => $this->options['queue'],
         );
+    }
+
+    private function setMessageMetadata(Message $message, Envelope $originalMessage): void
+    {
+        $configuration = $originalMessage->get(TransportConfiguration::class);
+
+        if (null === $configuration) {
+            return;
+        }
+
+        $metadata = $configuration->getMetadata();
+        $class = new \ReflectionClass($message);
+
+        foreach ($metadata as $key => $value) {
+            $setter = sprintf('set%s', ucfirst($key));
+            if (!$class->hasMethod($setter)) {
+                throw new MissingMessageMetadataSetterException($key, $setter, $class->getName());
+            }
+            $message->{$setter}($value);
+        }
     }
 }

Tests/AmqpContextManagerTest.php
@@ -1,0 +1,129 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Enqueue\MessengerAdapter\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Interop\Amqp\AmqpContext;
+use Interop\Amqp\AmqpQueue;
+use Interop\Amqp\AmqpTopic;
+use Interop\Amqp\Impl\AmqpBind;
+use Interop\Queue\Context;
+use Enqueue\MessengerAdapter\AmqpContextManager;
+
+class AmqpContextManagerTest extends TestCase
+{
+    private function getContextManager($topicName, $queueName, $topicType = null, $topicFlags = null, $queueBindingKey = null, $queueFlags = null)
+    {
+        $topicProphecy = $this->prophesize(AmqpTopic::class);
+        $topicProphecy->setType($topicType ?? AmqpTopic::TYPE_FANOUT)->shouldBeCalled();
+        if (null === $topicFlags) {
+            $topicProphecy->getFlags()->shouldBeCalled()->willReturn(AmqpTopic::FLAG_PASSIVE);
+        }
+        $topicProphecy->setFlags($topicFlags ?? (AmqpTopic::FLAG_DURABLE | AmqpTopic::FLAG_PASSIVE))->shouldBeCalled();
+        $topic = $topicProphecy->reveal();
+
+        $queueProphecy = $this->prophesize(AmqpQueue::class);
+        if (null === $queueFlags) {
+            $queueProphecy->getFlags()->shouldBeCalled()->willReturn(AmqpQueue::FLAG_PASSIVE);
+        }
+        $queueProphecy->setFlags($queueFlags ?? (AmqpQueue::FLAG_DURABLE | AmqpQueue::FLAG_PASSIVE))->shouldBeCalled();
+        $queue = $queueProphecy->reveal();
+
+        $bind = new AmqpBind($queue, $topic, $queueBindingKey);
+
+        $contextProphecy = $this->prophesize(AmqpContext::class);
+        $contextProphecy->createTopic($topicName)->shouldBeCalled()->willReturn($topic);
+        $contextProphecy->declareTopic($topic)->shouldBeCalled();
+        $contextProphecy->createQueue($queueName)->shouldBeCalled()->willReturn($queue);
+        $contextProphecy->declareQueue($queue)->shouldBeCalled();
+        $contextProphecy->bind($bind)->shouldBeCalled();
+        $context = $contextProphecy->reveal();
+
+        return new AmqpContextManager($context);
+    }
+
+    public function testDefaultEnsure()
+    {
+        $topicName = 'foo';
+        $queueName = 'bar';
+        $contextManager = $this->getContextManager($topicName, $queueName);
+
+        $this->assertTrue($contextManager->ensureExists(array(
+            'topic' => $topicName,
+            'topicOptions' => array('name' => $topicName),
+            'queue' => $queueName,
+            'queueOptions' => array('name' => $queueName),
+        )));
+    }
+
+    public function testCustomizedEnsure()
+    {
+        $topicName = 'foo';
+        $queueName = 'bar';
+        $topicType = 'topic';
+        $topicFlags = '8';
+        $queueFlags = 16;
+        $queueBindingKey = 'foo.#';
+        $contextManager = $this->getContextManager($topicName, $queueName, $topicType, $topicFlags, $queueBindingKey, $queueFlags);
+
+        $this->assertTrue($contextManager->ensureExists(array(
+            'topic' => $topicName,
+            'topicOptions' => array('name' => $topicName, 'type' => $topicType, 'flags' => $topicFlags),
+            'queue' => $queueName,
+            'queueOptions' => array('name' => $queueName, 'bindingKey' => $queueBindingKey, 'flags' => $queueFlags),
+        )));
+    }
+
+    public function testNotProcessedEnsure()
+    {
+        $topicName = 'foo';
+        $queueName = 'bar';
+
+        $contextProphecy = $this->prophesize(Context::class);
+        $context = $contextProphecy->reveal();
+
+        $contextManager = new AmqpContextManager($context);
+
+        $this->assertFalse($contextManager->ensureExists(array(
+            'topic' => $topicName,
+            'topicOptions' => array('name' => $topicName),
+            'queue' => $queueName,
+            'queueOptions' => array('name' => $queueName),
+        )));
+    }
+
+    public function testContextRetrieving()
+    {
+        $contextProphecy = $this->prophesize(Context::class);
+        $context = $contextProphecy->reveal();
+
+        $contextManager = new AmqpContextManager($context);
+
+        $this->assertSame($context, $contextManager->Context());
+    }
+
+    /* public function testExceptionRecovering()
+    {
+        $topicName = 'foo';
+        $queueName = 'bar';
+        $contextManager = $this->getContextManager($topicName, $queueName);
+
+        $exception = new \AMQPQueueException('', 404); // Class 'AMQPQueueException' not found
+
+        $this->assertTrue($contextManager->recoverException($exception, array(
+            'topic' => $topicName,
+            'topicOptions' => array('name' => $topicName),
+            'queue' => $queueName,
+            'queueOptions' => array('name' => $queueName),
+        )));
+    } */
+}